4ea5e4d95e39f392b5cebc8659c99c3ad89e4cbc
amaanlari <amaanlari@outlook.comm>
2024-02-01 14:39:13 +0530

    Added database files to gitignore

--- src/main/java/com/lari/restfulwebservices/controller/UserJpaController.java
+++ src/main/java/com/lari/restfulwebservices/controller/UserJpaController.java
@@ -43,12 +43,5 @@ public class UserJpaController {
         repository.deleteById(id);
     }
 
-    @GetMapping("/users/{id}/posts")
-    public List<Post> retrievePostsForUser(@PathVariable long id) {
-        Optional<User> user = repository.findById(id);
-        if (user.isEmpty()) {
-            throw new UserNotFoundException("id: "+id);
-        }
-        return user.get().getPosts();
-    }
+
 }
