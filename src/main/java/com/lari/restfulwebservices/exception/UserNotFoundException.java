7ba6f192dcbe37fa2079a1d19eb9abc051a93d75
amaanlari <amaanlari@outlook.comm>
2024-02-01 14:30:16 +0530

    Created custom UserNotFoundException and custom CustomResponseEntityExceptionHandler.

---
+++ src/main/java/com/lari/restfulwebservices/exception/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.lari.restfulwebservices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(
+        code = HttpStatus.NOT_FOUND
+)
+public class UserNotFoundException extends RuntimeException{
+    String message;
+
+    public UserNotFoundException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
